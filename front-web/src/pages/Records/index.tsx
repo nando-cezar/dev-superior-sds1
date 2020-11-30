258811164fe020a96e98e200f3e587fd81bdab92
veyson <luisfernando_cezar@hotmail.com>
2020-11-30 20:06:57 -0300

    wssts1 | implments view records;

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -3,20 +3,34 @@ import './styles.css';
 import axios from 'axios';
 import { RecordsResponse } from './types';
 import { formatDate } from './helpers';
+import Pagination from './Pagination';
+import { Link } from 'react-router-dom';
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'http://localhost:8080';
 
 const Records = () => {
 
   const [ recordsResponse , setRecordsResponse ] = useState<RecordsResponse>();
+  const [ activePage , setActivePage ] = useState(0);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/records?linePerPage=12`)
+    axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
     .then(response => setRecordsResponse(response.data));
-  }, []);
+  }, [activePage]);
+
+  const handlePageChange = (index: number) => {
+    setActivePage(index);
+  }
 
   return (
     <div className="page-container">
+      <div className="filters-container records-actions">
+        <Link to="/charts" >
+          <button className="action-filters">
+            VER GRÁFICOS
+          </button>
+        </Link>
+      </div>
       <table className="records-table" cellPadding="0" cellSpacing="0">
         <thead>
           <tr>
@@ -41,6 +55,11 @@ const Records = () => {
           ))}         
         </tbody>
       </table>
+      <Pagination
+        activePage={activePage}
+        goToPage={handlePageChange}
+        totalPages={recordsResponse?.totalPages}
+      />
     </div>
   );
 }
