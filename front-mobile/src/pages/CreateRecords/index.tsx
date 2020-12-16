9b9ee097c744e73ffc890b3ebc3de000d0ecc23e
veyson <luisfernando_cezar@hotmail.com>
2020-12-16 15:04:21 -0300

    ws-sts | finish view create-records;

--- front-mobile/src/pages/CreateRecords/index.tsx
+++ front-mobile/src/pages/CreateRecords/index.tsx
@@ -1,15 +1,160 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, TextInput, StyleSheet} from 'react-native';
 import Header from '../../components/Header';
+import PlatformCard from '../../components/PlatformCard';
+import { GamePlatform } from '../../components/PlatformCard/types';
+import RNPickerSelect from 'react-native-picker-select';
+import { FontAwesome5 as Icon} from '@expo/vector-icons';
+
+
+const placeholder = {
+  label: 'Selecione o game',
+  value: null
+}
 
 const CreateRecords = () => {
+
+  const[platform, setPlatform] = useState<GamePlatform>();
+  const[selectedGame, setSelectedGame] = useState('');
+
+  const handleChangePlatform = (selectedPlatform: GamePlatform) => {
+    setPlatform(selectedPlatform);
+  }
 
   return (
     <>
       <Header />
+      <View style={styles.container}>
+        <TextInput 
+          style={styles.inputText}
+          placeholder="Nome"
+          placeholderTextColor='#9E9E9E'
+        />
+        <TextInput 
+          keyboardType="numeric"
+          style={styles.inputText}
+          placeholder="Idade"
+          placeholderTextColor='#9E9E9E'
+          maxLength={3}
+        />
+        <View style={styles.platformContainer}>
+          <PlatformCard 
+            platform="XBOX"
+            icon="laptop"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+          <PlatformCard 
+            platform="PC"
+            icon="xbox"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+          <PlatformCard 
+            platform="PLAYSTATION"
+            icon="playstation"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+        </View>
+        <RNPickerSelect 
+          placeholder={placeholder}
+          onValueChange={value => {
+            setSelectedGame(value);
+          }}
+          items={[
+            {label:'' , value: ''}
+          ]}
+          style={pickerSelectStyles}
+          Icon={() => {
+            <Icon 
+              name="chevron-down" 
+              size={25}
+              color="#9E9E9E"
+      />
+          }}
+        />
+      </View>
       
     </>
   );
 }
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    color: '#ED7947',
+    paddingRight: 30,
+    fontFamily: "Play_700Bold",
+    height: 50
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    color: '#ED7947',
+    paddingRight: 30,
+    fontFamily: "Play_700Bold",
+    height: 50
+  },
+  placeholder: {
+    color: '#9E9E9E',
+    fontSize: 16,
+    fontFamily: "Play_700Bold",
+  },
+  iconContainer: {
+    top: 10,
+    right: 12,
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: '15%',
+    paddingRight: '5%',
+    paddingLeft: '5%',
+    paddingBottom: 50
+  },
+  inputText: {
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    color: '#ED7947',
+    fontFamily: "Play_700Bold",
+    fontSize: 16,
+    paddingLeft: 20,
+    marginBottom: 21
+  },
+  platformContainer: {
+    marginBottom: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  footer: {
+    marginTop: '15%',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#00D4FF',
+    flexDirection: 'row',
+    borderRadius: 10,
+    height: 60,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontFamily: "Play_700Bold",
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#0B1F34',
+  }
+});
 
 export default CreateRecords;
